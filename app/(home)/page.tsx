36b574c3ede466aee5992cc31cb9b0c00bcf7ad3
sorrel012 <wain1719@gmail.com>
2024-05-07 14:07:12 +0900

    [24.05.07] 영화 목록 페이지 구현

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,18 +1,28 @@
+import Link from 'next/link';
+
 export const metadata = {
   title: 'Home',
 };
 
-const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+export const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(URL);
   return await response.json();
 }
 
 async function HomePage() {
   const movies = await getMovies();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
 
 export default HomePage;
