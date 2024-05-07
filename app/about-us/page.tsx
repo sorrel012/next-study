ace68468c8278b0f0eb3e40fd16d9740a822ab25
sorrel012 <wain1719@gmail.com>
2024-05-07 10:22:54 +0900

    [24.05.07] Layout 사용

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from '../../components/navigation';
-
 function AboutUs() {
   return (
     <div>
-      <Navigation />
       <h1>About Us</h1>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from '../components/navigation';
-
 function Tomato() {
   return (
     <div>
-      <Navigation />
       <h1>Home</h1>
     </div>
   );
