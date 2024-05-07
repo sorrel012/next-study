67febdf442740eaa69d025f2a1cab4d2ff2192b0
sorrel012 <wain1719@gmail.com>
2024-05-07 11:25:01 +0900

    [24.05.07] data fetching 학습

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,23 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 function Tomato() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const getMovies = async () => {
+    const response = await fetch(
+      'https://nomad-movies.nomadcoders.workers.dev/movies',
+    );
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
