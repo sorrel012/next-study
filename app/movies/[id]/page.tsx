939906773f9d83595fce990440934aa4e17d7ded
sorrel012 <wain1719@gmail.com>
2024-05-07 14:32:56 +0900

    [24.05.07] data fetching parallel 구현

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -5,12 +5,19 @@ interface MovieProps {
 }
 
 async function getMovie(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 }
 
+async function getVideos(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
 async function MovieDetail({ params: { id } }: MovieProps) {
-  const movie = await getMovie(id);
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
 
   return <h1>{movie.title}</h1>;
 }
